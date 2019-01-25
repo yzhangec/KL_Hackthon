6633a0964a05e8859c57234547f2a8a41b471c52
Eason ZHANG <yzhangec@connect.ust.hk>
2019-01-26 01:06:53 +0800

    Clean Build

---
+++ Sample Code/WSDKTest/MainPage.xaml.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using DJI.WindowsSDK;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Media.Imaging;
+using ZXing;
+using ZXing.Common;
+using ZXing.Multi.QrCode;
+using Windows.Graphics.Imaging;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+namespace WSDKTest
+{
+    public sealed partial class MainPage : Page
+    {
+        private DJIVideoParser.Parser videoParser;
+        public WriteableBitmap VideoSource;
+
+        //Worker task (thread) for reading barcode
+        //As reading barcode is computationally expensive
+        private Task readerWorker = null;
+        private ISet<string> readed = new HashSet<string>();
+
+        private object bufLock = new object();
+        //these properties are guarded by bufLock
+        private int width, height;
+        private byte[] decodedDataBuf;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            //Listen for registration success
+            DJISDKManager.Instance.SDKRegistrationStateChanged += async (state, result) =>
+            {
+                if (state != SDKRegistrationState.Succeeded)
+                {
+                    var md = new MessageDialog(result.ToString());
+                    await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async ()=> await md.ShowAsync());
+                    return;
+                }
+                //wait till initialization finish
+                //use a large enough time and hope for the best
+                await Task.Delay(1000);
+                videoParser = new DJIVideoParser.Parser();
+                videoParser.Initialize();
+                videoParser.SetVideoDataCallack(0, 0, ReceiveDecodedData);
+                DJISDKManager.Instance.VideoFeeder.GetPrimaryVideoFeed(0).VideoDataUpdated += OnVideoPush;
+
+                await DJISDKManager.Instance.ComponentManager.GetFlightAssistantHandler(0, 0).SetObstacleAvoidanceEnabledAsync(new BoolMsg() { value = false });
+
+
+                await Task.Delay(5000);
+                GimbalResetCommandMsg resetMsg = new GimbalResetCommandMsg() { value = GimbalResetCommand.UNKNOWN };
+
+                await DJISDKManager.Instance.ComponentManager.GetGimbalHandler(0, 0).ResetGimbalAsync(resetMsg);
+            };
+            DJISDKManager.Instance.RegisterApp("Your App ID");
+        }
+
+        void OnVideoPush(VideoFeed sender, [ReadOnlyArray] ref byte[] bytes)
+        {
+            videoParser.PushVideoData(0, 0, bytes, bytes.Length);
+        }
+
+        void createWorker()
+        {
+            //create worker thread for reading barcode
+            readerWorker = new Task(async () =>
+            {
+                //use stopwatch to time the execution, and execute the reading process repeatedly
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                var reader = new QRCodeMultiReader();               
+                SoftwareBitmap bitmap;
+                HybridBinarizer binarizer;
+                while (true)
+                {
+                    try
+                    {
+                        lock(bufLock)
+                        {
+                            bitmap = new SoftwareBitmap(BitmapPixelFormat.Bgra8, width, height);
+                            bitmap.CopyFromBuffer(decodedDataBuf.AsBuffer());
+                        }
+                    }
+                    catch
+                    {
+                        //the size maybe incorrect due to unknown reason
+                        await Task.Delay(10);
+                        continue;
+                    }
+                    var source = new SoftwareBitmapLuminanceSource(bitmap);
+                    binarizer = new HybridBinarizer(source);
+                    var results = reader.decodeMultiple(new BinaryBitmap(binarizer));
+                    if (results != null && results.Length > 0)
+                    {
+                        await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                        {
+                            foreach (var result in results)
+                            {
+                                if (!readed.Contains(result.Text))
+                                {
+                                    readed.Add(result.Text);
+                                    Textbox.Text += result.Text + "\n";
+                                }
+                            }
+                        });
+                    }
+                    watch.Stop();
+                    int elapsed = (int)watch.ElapsedMilliseconds;
+                    //run at max 5Hz
+                    await Task.Delay(Math.Max(0, 200 - elapsed));
+                }
+            });
+        }
+
+        async void ReceiveDecodedData(byte[] data, int width, int height)
+        {
+            //basically copied from the sample code
+            lock (bufLock)
+            {
+                //lock when updating decoded buffer, as this is run in async
+                //some operation in this function might overlap, so operations involving buffer, width and height must be locked
+                if (decodedDataBuf == null)
+                {
+                    decodedDataBuf = data;
+                }
+                else
+                {
+                    if (data.Length != decodedDataBuf.Length)
+                    {
+                        Array.Resize(ref decodedDataBuf, data.Length);
+                    }
+                    data.CopyTo(decodedDataBuf.AsBuffer());
+                    this.width = width;
+                    this.height = height;
+                }
+            }
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                //dispatch to UI thread to do UI update (image)
+                //WriteableBitmap is exclusive to UI thread
+                if (VideoSource == null || VideoSource.PixelWidth != width || VideoSource.PixelHeight !=  height)
+                {
+                    VideoSource = new WriteableBitmap((int)width, (int)height);
+                    fpvImage.Source = VideoSource;
+                    //Start barcode reader worker after the first frame is received
+                    if (readerWorker == null)
+                    {
+                        createWorker();
+                        readerWorker.Start();
+                    }
+                }
+                lock (bufLock)
+                {
+                    //copy buffer to the bitmap and draw the region we will read on to notify the users
+                    decodedDataBuf.AsBuffer().CopyTo(VideoSource.PixelBuffer);
+                }
+                //Invalidate cache and trigger redraw
+                VideoSource.Invalidate();
+            });
+        }
+
+        private void Stop_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var throttle = 0;
+            var roll = 0;
+            var pitch = 0;
+            var yaw = 0;
+
+            try
+            {
+                if (DJISDKManager.Instance != null)
+                    DJISDKManager.Instance.VirtualRemoteController.UpdateJoystickValue(throttle, yaw, pitch, roll);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err);
+            }
+        }
+
+        private float throttle = 0;
+        private float roll = 0;
+        private float pitch = 0;
+        private float yaw = 0;
+
+        private async void Grid_KeyUp(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Windows.System.VirtualKey.W:
+                case Windows.System.VirtualKey.S:
+                    {
+                        throttle = 0;
+                        break;
+                    }
+                case Windows.System.VirtualKey.A:
+                case Windows.System.VirtualKey.D:
+                    {
+                        yaw = 0;
+                        break;
+                    }
+                case Windows.System.VirtualKey.I:
+                case Windows.System.VirtualKey.K:
+                    {
+                        pitch = 0;
+                        break;
+                    }
+                case Windows.System.VirtualKey.J:
+                case Windows.System.VirtualKey.L:
+                    {
+                        roll = 0;
+                        break;
+                    }
+                case Windows.System.VirtualKey.G:
+                    {
+                        var res = await DJISDKManager.Instance.ComponentManager.GetFlightControllerHandler(0, 0).StartTakeoffAsync();
+                        break;
+                    }
+                case Windows.System.VirtualKey.H:
+                    {
+                        var res = await DJISDKManager.Instance.ComponentManager.GetFlightControllerHandler(0, 0).StartAutoLandingAsync();
+                        break;
+                    }
+                
+            }
+
+            try
+            {
+                if (DJISDKManager.Instance != null)
+                    DJISDKManager.Instance.VirtualRemoteController.UpdateJoystickValue(throttle, yaw, pitch, roll);
+            }
+            catch (Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err);
+            }
+        }
+
+        private async void Grid_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Windows.System.VirtualKey.W:
+                    {
+                        throttle += 0.02f;
+                        if (throttle > 0.5f)
+                            throttle = 0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.S:
+                    {
+                        throttle -= 0.02f;
+                        if (throttle < -0.5f)
+                            throttle = -0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.A:
+                    {
+                        yaw -= 0.05f;
+                        if (yaw > 0.5f)
+                            yaw = 0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.D:
+                    {
+                        yaw += 0.05f;
+                        if (yaw < -0.5f)
+                            yaw = -0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.I:
+                    {
+                        pitch += 0.05f;
+                        if (pitch > 0.5)
+                            pitch = 0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.K:
+                    {
+                        pitch -= 0.05f;
+                        if (pitch < -0.5f)
+                            pitch = -0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.J:
+                    {
+                        roll -= 0.05f;
+                        if (roll < -0.5f)
+                            roll = -0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.L:
+                    {
+                        roll += 0.05f;
+                        if (roll > 0.5)
+                            roll = 0.5f;
+                        break;
+                    }
+                case Windows.System.VirtualKey.Number0:
+                    {
+                        GimbalAngleRotation rotation = new GimbalAngleRotation()
+                        {
+                            mode = GimbalAngleRotationMode.RELATIVE_ANGLE,
+                            pitch = 45,
+                            roll = 45,
+                            yaw = 45,
+                            pitchIgnored = false,
+                            yawIgnored = false,
+                            rollIgnored = false,
+                            duration = 0.5
+                        };
+
+                        System.Diagnostics.Debug.Write("pitch = 45\n");
+
+                        // Defined somewhere else
+                        var gimbalHandler = DJISDKManager.Instance.ComponentManager.GetGimbalHandler(0, 0);
+
+                        //angle
+                        //var gimbalRotation = new GimbalAngleRotation();
+                        //gimbalRotation.pitch = 45;
+                        //gimbalRotation.pitchIgnored = false;
+                        //gimbalRotation.duration = 5;
+                        //await gimbalHandler.RotateByAngleAsync(gimbalRotation);
+
+                        //Speed
+                        var gimbalRotation_speed = new GimbalSpeedRotation();
+                        gimbalRotation_speed.pitch = 10;
+                        await gimbalHandler.RotateBySpeedAsync(gimbalRotation_speed);
+
+                        //await DJISDKManager.Instance.ComponentManager.GetGimbalHandler(0,0).RotateByAngleAsync(rotation);
+
+                        break;
+                    }
+                case Windows.System.VirtualKey.P:
+                    {
+                        GimbalAngleRotation rotation = new GimbalAngleRotation()
+                        {
+                            mode = GimbalAngleRotationMode.RELATIVE_ANGLE,
+                            pitch = 45,
+                            roll = 45,
+                            yaw = 45,
+                            pitchIgnored = false,
+                            yawIgnored = false,
+                            rollIgnored = false,
+                            duration = 0.5
+                        };
+
+                        System.Diagnostics.Debug.Write("pitch = 45\n");
+
+                        // Defined somewhere else
+                        var gimbalHandler = DJISDKManager.Instance.ComponentManager.GetGimbalHandler(0, 0);
+
+                        //Speed
+                        var gimbalRotation_speed = new GimbalSpeedRotation();
+                        gimbalRotation_speed.pitch = -10;
+                        await gimbalHandler.RotateBySpeedAsync(gimbalRotation_speed);
+
+                        //await DJISDKManager.Instance.ComponentManager.GetGimbalHandler(0,0).RotateByAngleAsync(rotation);
+
+                        break;
+                    }
+            }
+
+            try
+            {
+                if (DJISDKManager.Instance != null)
+                    DJISDKManager.Instance.VirtualRemoteController.UpdateJoystickValue(throttle, yaw, pitch, roll);
+            }
+            catch(Exception err)
+            {
+                System.Diagnostics.Debug.WriteLine(err);
+            }
+        }
+    }
+}
